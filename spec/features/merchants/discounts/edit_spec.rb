ee4dee9651c9e1990068cf35d84fbe1aa0d22350
Kam Kennedy <kameronk013@gmail.com>
2023-11-12 17:53:26 -0700

    complete user story 5 and testing

--- spec/features/merchants/discounts/edit_spec.rb
+++ spec/features/merchants/discounts/edit_spec.rb
@@ -9,9 +9,36 @@ RSpec.describe "merchant discounts edit page" do
     @discount_3 = @merchant_2.discounts.create(percentage: 0.15, quantity: 5)
   end
 
-  it "allows a merchant to edit their discounts" do
-    visit (merchant_discount_show_path(@merchant_1, @discount_1))
-    expect(page).to have_content("Discount #{@discount_1} Show Page")
+  it "is linked from the discount show page" do
+    visit merchant_discount_path(@merchant_1, @discount_1)
+    expect(page).to have_content("Discount #{@discount_1.id} Show Page")
+    expect(page).to have_link("Edit Discount #{@discount_1.id} Details")
+    click_link("Edit Discount #{@discount_1.id} Details")
+    expect(current_path).to eq("/merchants/#{@merchant_1.id}/discounts/#{@discount_1.id}/edit")
+  end
 
+  it "is prefilled with discount attributes" do
+    visit edit_merchant_discount_path(@merchant_1, @discount_1)
+    expect(page).to have_field(:percentage, with: @discount_1.percentage)
+    expect(page).to have_field(:quantity, with: @discount_1.quantity)
+    expect(page).to have_button("Update Discount")
+  end
+
+  it "updates attributes and redirects to discount show page" do
+    visit edit_merchant_discount_path(@merchant_1, @discount_1)
+    fill_in "percentage", with: 0.6
+    fill_in "quantity", with: 40
+    click_button "Update Discount"
+    expect(current_path).to eq("/merchants/#{@merchant_1.id}/discounts/#{@discount_1.id}")
+    expect(page).to have_content("60% off, after 40 of any item purchased")
+    expect(page).to have_content("Discount successfully updated")
+  end
+
+  it "shows an error if a field is left blank" do
+    visit edit_merchant_discount_path(@merchant_1, @discount_1)
+    fill_in "percentage", with: ""
+    click_button "Update Discount"
+    expect(current_path).to eq("/merchants/#{@merchant_1.id}/discounts/#{@discount_1.id}/edit")
+    expect(page).to have_content("Discount update failed")
   end
 end
