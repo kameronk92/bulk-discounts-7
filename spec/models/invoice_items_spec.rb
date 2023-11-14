b5318ca67f5e334c8b98d3b63a4e3ae08cd0b851
Kam Kennedy <kameronk013@gmail.com>
2023-11-14 14:31:10 -0700

    complete tests, query, progress on view

--- spec/models/invoice_items_spec.rb
+++ spec/models/invoice_items_spec.rb
@@ -25,4 +25,20 @@ RSpec.describe InvoiceItem, type: :model do
       end
     end
   end
+
+  describe "#discount" do
+    it "returns the max discount an invoice item qualifies for" do
+      @invoice_8 = create(:invoice)
+      @merchant_3 = create(:merchant)
+      @discount_1 = @merchant_3.discounts.create(percentage: 0.25, quantity:51)
+      @item_8 = create(:item, merchant: @merchant_3, unit_price: 100)
+      @item_9 = create(:item, merchant: @merchant_3, unit_price: 1000)
+      invoice_item_1 = @item_8.invoice_items.create(quantity: 50, unit_price: 100, invoice: @invoice_8)
+      invoice_item_2 = @item_9.invoice_items.create(quantity: 100, unit_price: 1000, invoice: @invoice_8)
+      @discount_2 = @merchant_3.discounts.create(percentage: 0.1, quantity:20)
+require 'pry'; binding.pry
+      expect(@item_9.discount.id).to eq(@discount_1.id)
+      expect(@item_8.discount.id).to eq(@discount_2.id)
+    end
+  end
 end
